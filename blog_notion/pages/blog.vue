060b14a8425b3a286bd542d63c4b78ffdb2df8c9
MauicioSilva <redmaurosilva@gmail.com>
2022-11-04 11:22:16 -0500

    initial card style

--- blog_notion/pages/blog.vue
+++ blog_notion/pages/blog.vue
@@ -1,7 +1,63 @@
 
 <template>
-  <button @click="increment">Count is: {{ count }}</button>
-  <p>The messeage is: {{ msg[0].title }}</p>
+  <div>
+    <p>Lately on my code trip...</p>
+
+    <ul class="grid-cols-4">
+      <li v-for="post in posts">
+        <div
+          class="
+            container
+            bg-red-500
+            flex flex-row flex-wrap
+            basis-1/4
+            md:flex-row
+            mx-auto
+            mt-10
+            space-y-0
+            md:space-y-0
+          "
+        >
+          <div class="flex flex-col mb-32 space-y-12 md:w-1/2">
+            <!-- title -->
+            <h1
+              class="
+                max-w-md
+                text-4xl
+                font-bold
+                text-center
+                md:text-5xl md:text-left
+              "
+            >
+              {{ post.title }}
+            </h1>
+            <!-- caption -->
+            <p class="max-w-sm text-center text-darkGrayishBlue md:text-left">
+              Bring everyone together to build better productsBring everyone
+              together to build better productsBring everyone together to build
+              better productsBring everyone together to build better products
+            </p>
+            <div class="flex justify-center md:justify-start">
+              <a
+                href=""
+                class="
+                  p-3
+                  px-6
+                  py-2
+                  text-white
+                  bg-red-700
+                  rounded-full
+                  baseline
+                  hover:bg-brightRedLight
+                "
+                >Read more</a
+              >
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -34,8 +90,7 @@ export default {
       console.log("finding posts ..... ");
       const responseAPI = await $fetch("/api/notion");
       console.log("RESPONSE find post", responseAPI);
-      this.msg = responseAPI;
-      
+      this.posts = responseAPI;
     },
   },
 };
