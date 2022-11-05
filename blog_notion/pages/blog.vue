99e908bbe076dd55264b0bb30b5168df31047162
Mauricio <redmaurosilva@gail.com>
2022-11-04 23:50:41 -0500

    image binding

--- blog_notion/pages/blog.vue
+++ blog_notion/pages/blog.vue
@@ -1,60 +1,64 @@
-
 <template>
   <div>
     <p>Lately on my code trip...</p>
 
     <ul class="grid-cols-4">
       <li v-for="post in posts">
+        <!-- blog card -->
         <div
-          class="
-            container
-            bg-red-500
-            flex flex-row flex-wrap
-            basis-1/4
-            md:flex-row
-            mx-auto
-            mt-10
-            space-y-0
-            md:space-y-0
-          "
-        >
-          <div class="flex flex-col mb-32 space-y-12 md:w-1/2">
-            <!-- title -->
-            <h1
+        class="
+          container
+          flex flex-col-reverse
+          md:flex-row
+          items-center
+          px-6
+          mx-auto
+          mt-10
+          space-y-0
+          md:space-y-0
+        "
+      >
+        <!-- left item -->
+        <div class="flex flex-col mb-32 space-y-12 md:w-1/2">
+          <!-- title -->
+          <h1
+            class="
+              max-w-md
+              text-4xl
+              font-bold
+              text-center
+              md:text-5xl md:text-left
+            "
+          >
+{{post.title}}          </h1>
+          <!-- caption -->
+          <p class="max-w-sm text-center text-darkGrayishBlue md:text-left">
+            Bring everyone together to build better productsBring everyone
+            together to build better productsBring everyone together to build
+            better productsBring everyone together to build better products
+          </p>
+          <div class="flex justify-center md:justify-start">
+            <a
+              href=""
               class="
-                max-w-md
-                text-4xl
-                font-bold
-                text-center
-                md:text-5xl md:text-left
+                p-3
+                px-6
+                py-2
+                text-white
+                bg-red-700
+                rounded-full
+                baseline
+                hover:bg-brightRedLight
               "
+              >READ MORE</a
             >
-              {{ post.title }}
-            </h1>
-            <!-- caption -->
-            <p class="max-w-sm text-center text-darkGrayishBlue md:text-left">
-              Bring everyone together to build better productsBring everyone
-              together to build better productsBring everyone together to build
-              better productsBring everyone together to build better products
-            </p>
-            <div class="flex justify-center md:justify-start">
-              <a
-                href=""
-                class="
-                  p-3
-                  px-6
-                  py-2
-                  text-white
-                  bg-red-700
-                  rounded-full
-                  baseline
-                  hover:bg-brightRedLight
-                "
-                >Read more</a
-              >
-            </div>
           </div>
         </div>
+        <!-- image -->
+        <div class="mx-auto md:w-1/2">
+          <img class="object-fill" v-bind:src=post.coverImage />
+        </div>
+      </div>
       </li>
     </ul>
   </div>
@@ -69,6 +73,7 @@ export default {
       count: 0,
       posts: [],
       msg: "",
+      blogImageUrl:"https://www.notion.so/images/page-cover/rijksmuseum_rembrandt_1642.jpg"
     };
   },
   mounted() {
